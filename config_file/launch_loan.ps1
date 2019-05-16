18f4b792dad4bd3a0352179a7161a0995803348e
Guillaume <guil.rako@hotmail.fr>
2019-05-16 20:23:18 +0200

    update

--- config_file/launch_loan.ps1
+++ config_file/launch_loan.ps1
@@ -1,4 +1,4 @@
 ﻿cd ..\Algo_JL_Loan
 for ($i = 1; $i -lt 81; $i++) {
-    java -ea -cp ".\bin;.\librairies\peersim-1.0.5.jar;.\librairies\jep-2.3.0.jar;.\librairies\djep-1.0.0.jar" peersim.Simulator ".\src\config_$i.txt"
+    java -ea -cp ".\bin;.\librairies\peersim-1.0.5.jar;.\librairies\jep-2.3.0.jar;.\librairies\djep-1.0.0.jar" peersim.Simulator ".\config\config-$i.txt"
 }
