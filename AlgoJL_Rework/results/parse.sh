b0b83a662fde40660f530e90e43267dc31bdcc76
Callimard <guil.rako@hotmail.fr>
2019-05-23 13:44:43 +0200

    update

--- AlgoJL_Rework/results/parse.sh
+++ AlgoJL_Rework/results/parse.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-for i in {1..80}; do
+for i in {1..79}; do
 	NUM=$i
-	FILE="./"$1"/total/"$i"_total.csv"
-	PERCENT=$(tail -n 2 "$FILE")
-	RES="$NUM;$PERCENT"
+	FILE_PERCENT="./"$1"/total/"$i"_total.csv"
+	FILE_MESSAGE="./"$1"/other/messages_"$i".csv"
+	PERCENT=$(tail -n 2 "$FILE_PERCENT" | head -n 1)
+	NB_MESSAGE=$(tail -n 2 "$FILE_MESSAGE" | head -n 1)
+	RES="$NUM;$PERCENT;$NB_MESSAGE"
 	echo "$RES" >> "./$1/res.csv"
 done;
